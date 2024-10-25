bbb1205d1e12cd4b09d0b6a2595442c50ed8750d
scv829 <scv829@naver.com>
2024-10-25 18:55:26 +0900

    Feat:

---
+++ Assets/JaeseongHan/Scripts/CameraMove1.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraPos
+{
+    Up, Down, Right, Left
+}
+
+public class CameraMove1 : MonoBehaviour
+{
+    [SerializeField] private Transform _cubeTransform;  // Cube 오브젝트
+    [SerializeField] private Vector3 _offset = new Vector3(0, 5, -10);  // 카메라 기본 위치
+    [SerializeField] private float _mouseSensitivity = 200f;  // 마우스 민감도
+    [SerializeField] private float _rotationUpLimit = 50f;  // 카메라가 회전할 수 있는 최대 각도 (상하 제한)
+    [SerializeField] private float _rotationDownLimit = -20f;  // 카메라가 회전할 수 있는 최대 각도 (상하 제한)
+
+    private float _pitch = 0f;  // 위아래 각도 (X축 회전)
+    private float _yaw = 0f;  // 좌우 각도 (Y축 회전)
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // 매 프레임마다 호출되는 카메라 업데이트
+    void LateUpdate()
+    {
+        // 마우스 입력 받기
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+        // 마우스 입력을 기준으로 카메라의 각도 조정
+        _yaw += mouseX;
+        _pitch -= mouseY;
+
+        // 위아래 각도 제한 (큐브를 넘어가지 않도록)
+        _pitch = Mathf.Clamp(_pitch, _rotationDownLimit, _rotationUpLimit);
+
+        // 카메라의 회전 적용 (큐브를 중심으로 회전) 
+        Quaternion rotation = Quaternion.Euler(_pitch, _yaw, 0f);
+        // 카메라가 큐브를 중심으로 회전하도록 위치 계산
+        transform.position = _cubeTransform.position + rotation * _offset;  // Quaternion * position = postion의 각도 변경
+        // 카메라가 항상 큐브를 바라보도록 설정
+        transform.LookAt(_cubeTransform.position);
+    }
+    public CameraPos CameraPosition()
+    {
+        CameraPos _cameraPos = new CameraPos();
+        float tempX = transform.position.x - _cubeTransform.position.x;
+        float tempY = transform.position.y - (_offset.y + 1);
+
+        if (tempX + tempY > 6)
+            _cameraPos = CameraPos.Right;
+        else if (tempX - tempY < -6)
+            _cameraPos = CameraPos.Left;
+        else if (transform.position.z - _cubeTransform.position.z > 0)
+            _cameraPos = CameraPos.Up;
+        else
+            _cameraPos = CameraPos.Down;
+        //Debug.Log(_cameraPos);
+        return _cameraPos;
+    }
+}
---
+++ Assets/JaeseongHan/Scripts/CubeMove1.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CubePos
+{
+    Up, Down, Right, Left
+}
+
+public class CubeMove1 : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private BoxCollider _cubeUpCheck;
+    [SerializeField] private CameraMove1 _cameraMove;
+    CameraPos _cameraPos;
+    CubePos _cubePos;
+    bool IsRolling;
+
+    Bounds bound;
+    Vector3[] _moveDir = new Vector3[4];
+    Vector3 left, right, up, down;
+
+    void Start()
+    {
+        bound = GetComponent<BoxCollider>().bounds;
+        _moveDir[0] = new Vector3(0, -bound.size.y / 2, bound.size.z / 2);
+        _moveDir[1] = new Vector3(0, -bound.size.y / 2, -bound.size.z / 2);
+        _moveDir[2] = new Vector3(bound.size.x / 2, -bound.size.y / 2, 0);
+        _moveDir[3] = new Vector3(-bound.size.x / 2, -bound.size.y / 2, 0);
+    }
+
+    void Update()
+    {
+        _cameraPos = _cameraMove.CameraPosition();
+        // Move Forward
+        float _HMove = Input.GetAxisRaw("Horizontal");
+        float _VMove = Input.GetAxisRaw("Vertical");
+
+        if (_HMove == 0 && _VMove == 0) return;
+
+        if (_VMove == 1)
+        {
+            switch (_cameraPos)
+            {
+                case CameraPos.Up:
+                    _cubePos = CubePos.Down;
+                    break;
+                case CameraPos.Down:
+                    _cubePos = CubePos.Up;
+                    break;
+                case CameraPos.Right:
+                    _cubePos = CubePos.Left;
+                    break;
+                case CameraPos.Left:
+                    _cubePos = CubePos.Right;
+                    break;
+            }
+        }
+
+        // Move Backwards
+        else if (_VMove == -1)
+        {
+            switch (_cameraPos)
+            {
+                case CameraPos.Up:
+                    _cubePos = CubePos.Up;
+                    break;
+                case CameraPos.Down:
+                    _cubePos = CubePos.Down;
+                    break;
+                case CameraPos.Right:
+                    _cubePos = CubePos.Right;
+                    break;
+                case CameraPos.Left:
+                    _cubePos = CubePos.Left;
+                    break;
+            }
+        }
+
+        // Move Right
+        else if (_HMove == 1)
+        {
+            switch (_cameraPos)
+            {
+                case CameraPos.Up:
+                    _cubePos = CubePos.Left;
+                    break;
+                case CameraPos.Down:
+                    _cubePos = CubePos.Right;
+                    break;
+                case CameraPos.Right:
+                    _cubePos = CubePos.Up;
+                    break;
+                case CameraPos.Left:
+                    _cubePos = CubePos.Down;
+                    break;
+            }
+        }
+
+        // Move Left
+        else if (_HMove == -1)
+        {
+            switch (_cameraPos)
+            {
+                case CameraPos.Up:
+                    _cubePos = CubePos.Right;
+                    break;
+                case CameraPos.Down:
+                    _cubePos = CubePos.Left;
+                    break;
+                case CameraPos.Right:
+                    _cubePos = CubePos.Down;
+                    break;
+                case CameraPos.Left:
+                    _cubePos = CubePos.Up;
+                    break;
+            }
+        }
+        if (!IsRolling)
+            StartCoroutine(Roll(_cubePos));
+    }
+
+    IEnumerator Roll(CubePos cubePos)
+    {
+        Vector3 positionToRotation = _moveDir[(int)cubePos];
+        switch (cubePos)
+        {
+            case CubePos.Up:
+                _cubeUpCheck.transform.position += new Vector3(0, 0, 1);
+                break;
+            case CubePos.Down:
+                _cubeUpCheck.transform.position += new Vector3(0, 0, -1);
+                break;
+            case CubePos.Right:
+                _cubeUpCheck.transform.position += new Vector3(1, 0, 0);
+                break;
+            case CubePos.Left:
+                _cubeUpCheck.transform.position += new Vector3(-1, 0, 0);
+                break;
+        }
+        IsRolling = true;
+        _cubeUpCheck.enabled = false;
+        float angle = 0;
+        Vector3 point = transform.position + positionToRotation;
+        Vector3 axis = Vector3.Cross(Vector3.up, positionToRotation).normalized;
+
+        while (angle < 90f)
+        {
+            float angleSpeed = Time.deltaTime * _rotationSpeed;
+            transform.RotateAround(point, axis, angleSpeed);
+            angle += angleSpeed;
+            yield return null;
+        }
+
+        transform.RotateAround(point, axis, 90 - angle);
+        IsRolling = false;
+        _cubeUpCheck.enabled = true;
+    }
+}
