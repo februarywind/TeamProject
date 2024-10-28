5b3cb731fc1b2b8d061114aaac5982ae8c87e399
februarywind <89524114+februarywind@users.noreply.github.com>
2024-10-28 12:16:02 +0900

    Feat : Cube Blocking Block #12
    큐브의 특정 방향 이동을 막는 프리팹 제작

---
+++ Assets/KwonMinGyu/Script/CubeMoveBlocking4.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoveBlocking4 : MonoBehaviour
+{
+    [SerializeField] private CubePos4[] _blockingDir;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+        CubeMove4 cubeMove4 = other.GetComponent<CubeMove4>();
+        cubeMove4.IsBlockingForward = true;
+        cubeMove4.BlockingDir = _blockingDir;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player") return;
+        CubeMove4 cubeMove4 = other.GetComponent<CubeMove4>();
+        cubeMove4.IsBlockingForward = false;
+    }
+}
