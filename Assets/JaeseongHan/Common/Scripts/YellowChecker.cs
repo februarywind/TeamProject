6ab334392478c5671c6c45dad4772f4135bd1589
scv829 <scv829@naver.com>
2024-10-31 17:03:12 +0900

    Fix: 노란색 스탬프 벽 인식 관련 버그 수정
    - 노란색 스탬프 벽 인식 관련 버그 수정
    - 바로 파괴가 아니라 우선 오브젝트를 내리고 그 후에 파괴로 고침
    
    Related to: #75

--- Assets/JaeseongHan/Common/Scripts/YellowChecker.cs
+++ Assets/JaeseongHan/Common/Scripts/YellowChecker.cs
@@ -12,14 +12,21 @@ public class YellowChecker : MonoBehaviour
     [SerializeField] float distance;        // 레이캐스트 거리
     [SerializeField] GameObject destroyTarget;  // 능력 사용 중 파괴할 타겟
 
+    [SerializeField] UnityEvent destroyCubeEvent;   // 파괴할 타겟에게 파괴하라고 명령하는 이벤트
+
     /// <summary>
     /// 돌진 가능 여부를 가져오는 프로퍼티
     /// </summary>
     public bool CanMove { get { return canMove; } private set { } }
-    public void DestroyTarget() { if(destroyTarget is not null) Destroy(destroyTarget); }
+    public void DestroyTarget()
+    {
+        if (destroyTarget is not null) destroyCubeEvent?.Invoke();
+    }
 
     public void CheckRay()
     {
+        // 일단 초기화 
+        destroyCubeEvent.RemoveAllListeners();
         RaycastHit hit;
 
         // Ray를 checker의 아래 방향으로 발사하고
@@ -54,6 +61,13 @@ public class YellowChecker : MonoBehaviour
             destroyTarget = other.gameObject;
             // 움직여서 파괴할 수 있으니 움직이 가능으로 설정
             canMove = true;
+
+            DestroyCube script = other.gameObject.GetComponent<DestroyCube>();
+            // 혹시 모를 한번 더 검증
+            if (script is not null)
+            {
+                destroyCubeEvent.AddListener(script.Remove);
+            }
         }
         else canMove = false;
     }
