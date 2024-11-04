7a4978dd52a950cf8b89278e1d1bc1b35f5490d5
KimSu-70 <rlatngh4770@naver.com>
2024-11-04 12:08:32 +0900

    feat : 사운드 개선 및 1스테이지 수정
    도어 사운드 추가 및 1스테이지 수정

--- Assets/KimSuHo/AudioTest/GateOpen.cs
+++ Assets/KimSuHo/AudioTest/GateOpen.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GateOpen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Animator animator;
+    private bool audioPlay = false;
     void Start()
     {
-        
+        // 현재 게임 오브젝트에서 Animator 컴포넌트를 가져옴
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Animator가 존재하고 활성화된 경우
+        if (animator != null && animator.enabled && !audioPlay)
+        {
+            AudioManager.Instance.PlaySfx(AudioManager.Sfx.GateOpen);
+            audioPlay = true;
+            StartCoroutine(Opens());
+        }
+    }
+
+    IEnumerator Opens ()
+    {
+        yield return new WaitForSeconds(0.5f);
+        this.enabled = false;
     }
 }
