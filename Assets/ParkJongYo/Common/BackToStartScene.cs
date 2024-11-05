96fcd2ae0894aff3aaa20938cde32065a16ed93a
JongYo-Park <kioasdy1@gmail.com>
2024-11-05 16:11:05 +0900

    [Fix] mouse cursor issue
    마우스 커서 사라짐 문제 해결

--- Assets/ParkJongYo/Common/BackToStartScene.cs
+++ Assets/ParkJongYo/Common/BackToStartScene.cs
@@ -11,6 +11,10 @@ public class BackToStartScene : MonoBehaviour
         // ESC 키를 눌렀을 때 로비 씬으로 전환
         if (Input.anyKeyDown)
         {
+            // 마우스 커서를 다시 활성화
+            Cursor.lockState = CursorLockMode.None; // 커서를 잠금 해제
+            Cursor.visible = true; // 커서를 보이도록 설정
+
             SceneManager.LoadScene(startSceneName);
         }
     }
